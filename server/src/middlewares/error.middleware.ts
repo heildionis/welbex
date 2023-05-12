549aee6d287a2d9a029b127534afffc82a05b8e2
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-05-12 22:25:53 +0300

    Middleware: fix bugs

--- server/src/middlewares/error.middleware.ts
+++ server/src/middlewares/error.middleware.ts
@@ -1,16 +1,17 @@
-import { NextFunction, Request, Response } from 'express';
+import { Request, Response } from 'express';
 import { ApiError } from '../exception/ApiError.js';
 import colors from 'colors';
 
+// Error handling middleware
 export const errorMiddleware = (
 	err: any,
 	req: Request,
 	res: Response,
-	next: NextFunction
+	next: Function
 ) => {
 	console.log(colors.red(err));
 
-	if (err instanceof ApiError) {
+	if (!(err instanceof ApiError)) {
 		return res.status(err.status).json(err.message);
 	}
 
