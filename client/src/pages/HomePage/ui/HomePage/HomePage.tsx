a477db61adba36f07a1675de1f3af2970a92aafd
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-05-14 03:10:14 +0300

    Fix

--- client/src/pages/HomePage/ui/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/ui/HomePage/HomePage.tsx
@@ -57,11 +57,7 @@ export const HomePage = memo((props: HomePageProps) => {
 			<PostList
 				isLoading={isLoading}
 				renderOptions={renderPostOptions}
-				posts={
-					postsWithCount?.posts
-						? [...postsWithCount.posts].reverse()
-						: []
-				}
+				posts={postsWithCount?.posts}
 				className={cls.list}
 			/>
 			<Pagination
